0885ebe1179537a6fbd3b1b96ede0ecd2c552a92
KaniNayuta27 <kani@users.noreply.github.com>
2026-09-01 16:04:29 +0000

    0.9.40/1.1.37 placeholder only if unselected; rehello after exe restart

--- functions/update/apk.js
+++ functions/update/apk.js
@@ -1,3 +1,3 @@
 export async function onRequest() {
-  return Response.redirect("https://tapsprite.pages.dev/tapsprite0-9-39.apk", 302);
+  return Response.redirect("https://tapsprite.pages.dev/tapsprite0-9-40.apk", 302);
 }
