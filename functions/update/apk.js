fac5e584b19a0443947646e030c86995d465dc7f
KaniNayuta27 <kani@users.noreply.github.com>
2026-09-01 15:08:47 +0000

    0.9.37/1.1.35 ignore stale hello after offline

--- functions/update/apk.js
+++ functions/update/apk.js
@@ -1,3 +1,3 @@
 export async function onRequest() {
-  return Response.redirect("https://tapsprite.pages.dev/tapsprite0-9-36.apk", 302);
+  return Response.redirect("https://tapsprite.pages.dev/tapsprite0-9-37.apk", 302);
 }
